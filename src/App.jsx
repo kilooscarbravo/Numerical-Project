34acaa728198db26958e114c9fca1278dd8bb363
kilooscarbravo <kobpoy9@gmail.com>
2023-11-10 13:06:02 +0700

    fix some code

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,16 @@ import BisectionTonkaew from './components/BisectionTonkaew'
 const App = () => {
   return (
     <div>
-              <BisectionKob/>
-              <BisectionTonkaew />
 
-      {/* <Navbar></Navbar>
+      <Navbar></Navbar>
       <main>
         <div>
             <div id='home'>
               <Home />
             </div>
             <div id='bisection'>
-              <Bisection />            </div>
+              <Bisection />            
+            </div>
             <div id='graphical'>
               <Graphical />
             </div>
@@ -36,44 +35,8 @@ const App = () => {
               <Newton />
             </div>
         </div>
-      </main> */}
+      </main>
     </div>
   )
 }
 export default App
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
